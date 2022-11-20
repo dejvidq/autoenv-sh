5478c9e830cdc6042988c1fd1210232795437198
dejvidq <15433565+dejvidq@users.noreply.github.com>
2022-11-20 22:43:21 +0100

    Added commands for creating and adding autoenvs and also to show config

--- autoenv.sh
+++ autoenv.sh
@@ -21,11 +21,8 @@ read_config() {
 
 write_config() {
 	cat /dev/null > "${CONFIG_FILE}"
-	for i in "${!CONFIG[@]}"
-    do
-        key="$i"
-        value="${CONFIG[$i]}"
-		echo "${key}=${value}" >> "${CONFIG_FILE}"
+	for venv_path venv_name in "${(@kv)CONFIG}"; do
+		echo "${venv_path}=${venv_name}" >> "${CONFIG_FILE}"
     done
 }
 
@@ -46,4 +43,44 @@ smart_venv_activate() {
 		done
 	fi
 	[[ $venv_activate = 0 && "${#VIRTUAL_ENV}" -gt 0 ]] && deactivate
+}
+
+new-autoenv() {
+    local _name="$1"
+	local _python="${2-python}"
+	local _path="${3-$(pwd)}"
+
+	_python=$(which ${_python})
+	if [[ -d ${_path} ]]; then
+		_path=$(realpath ${_path})
+	else
+		print -P "%F{yellow}WARNING! Path '${_path}' does not exist or it's not a directory. Using current path instead: '$(pwd)'!%f\n"
+		_path=$(pwd)
+	fi
+	_path=${_path%%/}
+	read_config
+	${_python} -m venv "$CONFIG_PATH/${_name}"
+	CONFIG[${_path}]=${_name}
+	write_config
+}
+
+add-autoenv() {
+    local _name="$1"
+	local _path="$3"
+
+	read_config
+	if [[ -d "${CONFIG_PATH}/${_name}" ]]; then
+	    CONFIG[${_path}]=${_name}
+	else
+	    print -P "%F{red}Virtualenv '${_name}' does not exist!%f\n"
+	fi
+	write_config
+}
+
+get-autoenv-config() {
+    column -t -s"=" -N "Location,Name" $CONFIG_FILE
+}
+
+get-all-autoenvs() {
+    column -t -s"=" -N "Location,Name" -H "Location" $CONFIG_FILE
 }
